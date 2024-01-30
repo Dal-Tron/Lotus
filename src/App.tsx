422511e3fb44840a5a24d988bdf98330e738ea26
digger0702 <hurricanehunter0702@gmail.com>
2024-01-30 11:17:50 +0900

    starter

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import supabase from "./services/db";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-red-600">
+      CRA + Tailwind CSS + TypeScript + Supabase Starter
+    </h1>
+  );
+}
+
+export default App;
