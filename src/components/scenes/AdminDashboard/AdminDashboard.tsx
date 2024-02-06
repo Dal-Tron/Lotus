b5ef2465609541499a37212adae89c53425aee6f
digger0702 <hurricanehunter0702@gmail.com>
2024-02-06 16:10:33 +0900

    sidebar update

--- src/components/scenes/AdminDashboard/AdminDashboard.tsx
+++ src/components/scenes/AdminDashboard/AdminDashboard.tsx
@@ -77,7 +77,7 @@ const AdminDashboard = () => {
           <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
               <div className="overflow-hidden">
-                <table className="min-w-full text-left text-sm font-light">
+                <table className="min-w-full text-left text-sm font-light overflow-x-scroll">
                   <thead className="border-b font-medium dark:border-neutral-500">
                     <tr>
                       <th scope="col" className="px-6 py-4">
